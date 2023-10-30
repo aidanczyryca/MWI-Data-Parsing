f01dae7f238d8dfab7b1198465544ffc4766655f
Aidan Czyryca <aczyryca@gmail.com>
2023-10-30 04:54:47 -0400

    Acquiring the JSON

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import requests
+import json
+
 def main():
-    print("Hello World!")
+    file = open("MWIdata.json", "w+")
+    file.write((requests.get('https://raw.githubusercontent.com/silent1b/MWIData/main/init_client_info.json')).text)
+    #response.json()
+    #html_data = response.text
+    
 
 if __name__ == "__main__":
     main()
