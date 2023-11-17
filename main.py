7e95211669b252f5179b947a1073c6ca760b37c6
Aidan Czyryca <aczyryca@gmail.com>
2023-11-17 02:11:49 -0500

    new txt file and better filtered text

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import requests
 import json
 
 def main():
-    file = open("MWIdata.json", "w+")
-    file.write(((requests.get('https://raw.githubusercontent.com/silent1b/MWIData/main/init_client_info.json')).text))
+    rawFile = open("MWIdata.json", "w+")
+    rawFile.write(((requests.get('https://raw.githubusercontent.com/silent1b/MWIData/main/init_client_info.json')).text))
+    filteredFile = open("HouseRooms.txt", "w+")
     #print(file.read())
     #full = (((requests.get('https://raw.githubusercontent.com/silent1b/MWIData/main/init_client_info.json')).text)[2631373:2696862]).replace(",", " ").replace("\"","")
     #print(full.find("{\"/house_rooms/archery_range\""))
@@ -11,22 +12,28 @@ def main():
     #full = full[full.find("{\"/house_rooms/archery_range\""):full.find("\"count\":2000}]},\"sortIndex\":5}}")]
     #full = full[2631373:2696862]
     full = takeOut((requests.get('https://raw.githubusercontent.com/silent1b/MWIData/main/init_client_info.json')).text)
-    print(full)
+    filteredFile.write(full)
     
 def takeOut(text):
     new = text[2631373:2696862]
-    for char in new:
-        new = new.replace("sortIndex\":15", "\n")
-        new = new.replace(",", " ")
-        new = new.replace("\"","")
-        new = new.replace("/", "")
-        new = new.replace("items", "")
-        new = new.replace("itemHrid", "")
-        new = new.replace(":", "")
-        new = new.replace("[", "")
-        new = new.replace("]", "")
-        new = new.replace("}", "")
-        new = new.replace("{", "")
+    new = new.replace("sortIndex", "\n\n")
+    new = new.replace(",", " ")
+    new = new.replace("\"","")
+    new = new.replace("/", "")
+    new = new.replace("items", "")
+    new = new.replace("itemHrid", "")
+    new = new.replace(":", "")
+    new = new.replace("[", "")
+    new = new.replace("]", "")
+    new = new.replace("}", "")
+    new = new.replace("{", "")
+    for char in "123456789":
+        new = new.replace(char+"coin","\nRoom Level "+char+":\tcoin")
+    new = new.replace(" count", "-")
+    
+    #roomList = new.split("\n\n")
+    #print(roomList)
+    #new = "Archery Range" + new[706:]
     return new
 
 
