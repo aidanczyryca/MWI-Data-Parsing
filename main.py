23abbbf200e7212c81e220dc47e4a58b0aed95c9
Aidan Czyryca <aczyryca@gmail.com>
2023-11-19 04:26:33 -0500

    write to a file instead of print
    print does not allow that many chars

--- main.py
+++ main.py
@@ -5,16 +5,10 @@ def main():
     rawFile = open("MWIdata.json", "w+")
     rawFile.write(((requests.get('https://raw.githubusercontent.com/silent1b/MWIData/main/init_client_info.json')).text))
     filteredFile = open("HouseRooms.txt", "w+")
-    #print(file.read())
-    #full = (((requests.get('https://raw.githubusercontent.com/silent1b/MWIData/main/init_client_info.json')).text)[2631373:2696862]).replace(",", " ").replace("\"","")
-    #print(full.find("{\"/house_rooms/archery_range\""))
-    #print(full.find("\"count\":2000}]},\"sortIndex\":5}}"))
-    #full = full[full.find("{\"/house_rooms/archery_range\""):full.find("\"count\":2000}]},\"sortIndex\":5}}")]
-    #full = full[2631373:2696862]
-    full = takeOut((requests.get('https://raw.githubusercontent.com/silent1b/MWIData/main/init_client_info.json')).text)
-    filteredFile.write(full)
     
-def takeOut(text):
+    filteredFile.write(filterRooms((requests.get('https://raw.githubusercontent.com/silent1b/MWIData/main/init_client_info.json')).text))
+    
+def filterRooms(text):
     new = text[2631373:2696862]
     new = new.replace("sortIndex", "\n\n")
     new = new.replace(",", " ")
@@ -30,7 +24,6 @@ def takeOut(text):
     for char in "123456789":
         new = new.replace(char+"coin","\nRoom Level "+char+":\tcoin")
     new = new.replace(" count", "-")
-    
     #roomList = new.split("\n\n")
     #print(roomList)
     #new = "Archery Range" + new[706:]
